2d08a090b8521ece11fd415d3789f0981b208591
Siavash Golchoobian <siavash.golchoobian@gmail.com>
2022-10-31 12:20:33 +0330

    SQL Reg Path Correction

--- _Maintenance Tasks/Once Configuration/SQL_SetErrorLogFileCount.sql
+++ _Maintenance Tasks/Once Configuration/SQL_SetErrorLogFileCount.sql
@@ -1,6 +1,7 @@
+--ATTENTION: Replace <yourInstance> with your instance name like MSSQL15.Node or MSSQLServer
 USE [master]
 GO
-EXEC xp_instance_regwrite N'HKEY_LOCAL_MACHINE', N'Software\Microsoft\MSSQLServer\MSSQLServer', N'ErrorLogSizeInKb', REG_DWORD, 10240
+EXEC xp_instance_regwrite N'HKEY_LOCAL_MACHINE', N'Software\Microsoft\<yourInstance>\MSSQLServer', N'ErrorLogSizeInKb', REG_DWORD, 10240
 GO
-EXEC xp_instance_regwrite N'HKEY_LOCAL_MACHINE', N'Software\Microsoft\MSSQLServer\MSSQLServer', N'NumErrorLogs', REG_DWORD, 12
+EXEC xp_instance_regwrite N'HKEY_LOCAL_MACHINE', N'Software\Microsoft\<yourInstance>\MSSQLServer', N'NumErrorLogs', REG_DWORD, 12
 GO
--- _Maintenance Tasks/Once Configuration/SQL_SetRecoveryInterval.sql
+++ _Maintenance Tasks/Once Configuration/SQL_SetRecoveryInterval.sql
@@ -1,3 +1,9 @@
+USE master ;  
+GO  
+EXEC sp_configure 'show advanced options', 1 ;  
+GO  
+RECONFIGURE  
+GO 
 EXEC sys.sp_configure N'recovery interval (min)', N'1'
 GO
 RECONFIGURE WITH OVERRIDE
