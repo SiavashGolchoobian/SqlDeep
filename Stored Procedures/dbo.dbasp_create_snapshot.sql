9a49f66cc7bc80a9e4febfadf3fcf9a916b71ebf
Siavash Golchoobian <siavash.golchoobian@gmail.com>
2023-01-07 11:55:47 +0330

    correcting snapshot creation for multiple databases

--- Stored Procedures/dbo.dbasp_create_snapshot.sql
+++ Stored Procedures/dbo.dbasp_create_snapshot.sql
@@ -8,7 +8,7 @@ GO
 -- =============================================
 -- Author:		<Golchoobian>
 -- Create date: <1/21/2015>
--- Version:		<3.0.0.0>
+-- Version:		<3.0.0.1>
 -- Description:	<Create database snapshot>
 -- Input Parameters:
 --	@DatabaseNames:				'<ALL_USER_DATABASES>' or '<ALL_SYSTEM_DATABASES>' or '<ALL_DATABASES>' or 'dbname1,dbname2,...,dbnameN'
@@ -29,8 +29,6 @@ BEGIN
 	DECLARE @SqlCmd nvarchar(MAX);
 	DECLARE @myCursor Cursor;
 	
-	SET @SqlCmd=CAST(N'' AS NVARCHAR(MAX))
-
 	SET @myCursor=CURSOR For
 		Select [Name] FROM [dbo].[dbafn_database_list](@DatabaseNames,1,1,1,0,0)
 		
@@ -38,6 +36,7 @@ BEGIN
 	FETCH NEXT FROM @myCursor INTO @Database_Name
 			WHILE @@FETCH_STATUS=0
 			BEGIN
+				SET @SqlCmd=CAST(N'' AS NVARCHAR(MAX))
 				DELETE FROM @SnapshotArray
 				INSERT INTO @SnapshotArray
 				SELECT 
--- _Maintenance Tasks/Once Configuration/SQL_SetErrorLogFileCount.sql
+++ _Maintenance Tasks/Once Configuration/SQL_SetErrorLogFileCount.sql
@@ -1,7 +1,7 @@
 --ATTENTION: Replace <yourInstance> with your instance name like MSSQL15.Node or MSSQLServer
 USE [master]
 GO
-EXEC xp_instance_regwrite N'HKEY_LOCAL_MACHINE', N'Software\Microsoft\<yourInstance>\MSSQLServer', N'ErrorLogSizeInKb', REG_DWORD, 10240
+EXEC xp_instance_regwrite N'HKEY_LOCAL_MACHINE', N'SOFTWARE\Microsoft\Microsoft SQL Server\<yourInstance,Like:MSSQL15.NODE>\MSSQLServer', N'ErrorLogSizeInKb', REG_DWORD, 10240
 GO
-EXEC xp_instance_regwrite N'HKEY_LOCAL_MACHINE', N'Software\Microsoft\<yourInstance>\MSSQLServer', N'NumErrorLogs', REG_DWORD, 12
+EXEC xp_instance_regwrite N'HKEY_LOCAL_MACHINE', N'SOFTWARE\Microsoft\Microsoft SQL Server\<yourInstance,Like:MSSQL15.NODE>\MSSQLServer', N'NumErrorLogs', REG_DWORD, 12
 GO
