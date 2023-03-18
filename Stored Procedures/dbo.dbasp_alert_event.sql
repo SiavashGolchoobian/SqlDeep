bf306091b306c55fa273de187302efa753a22b72
Siavash Golchoobian <siavash.golchoobian@gmail.com>
2023-03-18 12:14:02 +0330

    resync

--- Stored Procedures/dbo.dbasp_alert_event.sql
+++ Stored Procedures/dbo.dbasp_alert_event.sql
@@ -1,58 +1,110 @@
-SET QUOTED_IDENTIFIER ON
-GO
-SET ANSI_NULLS ON
-GO
-CREATE PROCEDURE [dbo].[dbasp_alert_event]
-@AlertType VARCHAR (50)=NULL, @AlertText XML=NULL, @LogAlertToTable BIT=1, @LogAlertToSqlLog BIT=1, @PrintAlertToConsole BIT=1, @ReturnAlertAsResultset BIT=0, @EmailAlert BIT=0, @MailList VARCHAR (255)=NULL
+-- Stored Procedure
+
+-- =============================================
+-- Author:		<Golchoobian>
+-- Create date: <03/13/2017>
+-- Version:		<3.0.0.0>
+-- Description:	<Send or Save messages as alerts>
+-- Input Parameters:
+--	@AlertType:				'...'	//Any custom message as subject or category
+--	@AlertText				'...'	//Any custom xml message as detail description
+--	@LogAlertToTable		0 or 1	//1 to Log message in trace.Events table or 0 to skip it
+--	@LogAlertToSqlLog:		0 or 1	//1 to Raise an error on SQL log or 0 to skip it
+--	@PrintAlertToConsole:	0 or 1	//1 to Print output result or 0 to skip it
+--	@ReturnAlertAsResultset:0 or 1	//1 to Return output result grid or 0 to skip it
+--	@EmailAlert:			0 or 1	//1 to Email alert to @MailList or 0 to skip it
+--	@MailList:				'...'	//mail list with semicolon seperated list for @EmailAlert true condition
+-- =============================================
+CREATE PROCEDURE [dbo].[dbasp_alert_event] (
+	@AlertType VARCHAR(50)=Null,
+	@AlertText XML=Null,
+	@LogAlertToTable BIT=1,
+	@LogAlertToSqlLog BIT=1,
+	@PrintAlertToConsole BIT=1,
+	@ReturnAlertAsResultset BIT=0,
+	@EmailAlert BIT=0,
+	@MailList VARCHAR(255)=NULL
+	)
 AS
 BEGIN
-    DECLARE @myAlertTime AS DATETIME;
-    DECLARE @myNewLine AS NVARCHAR (10);
-    SET @myAlertTime = GETDATE();
-    SET @myNewLine = CHAR(13) + CHAR(10);
-    SET @AlertType = ISNULL(@AlertType, N'UNKNOWN');
-    SET @AlertText = ISNULL(@AlertText, CAST (N'<UNKNOWN/>' AS XML));
-    SET @AlertText = CAST (N'<instance_name>' + CAST (@@SERVERNAME AS NVARCHAR (255)) + N'</instance_name>' + @myNewLine + CAST (@AlertText AS NVARCHAR (MAX)) AS XML);
-    IF @LogAlertToTable = 1
-        BEGIN
-            INSERT  INTO [trace].[Events] ([AlertTime], [AlertType], [AlertText])
-            VALUES                       (@myAlertTime, @AlertType, @AlertText);
-        END
-    IF @LogAlertToSqlLog = 1
-        BEGIN
-            DECLARE @myErrorMessage AS NVARCHAR (4000);
-            DECLARE @myErrorState AS INT;
-            SET @myErrorMessage = @AlertType + N' on ' + CAST (@myAlertTime AS NVARCHAR (50)) + @myNewLine + +CAST (@AlertText AS NVARCHAR (3600));
-            SET @myErrorState = 1;
-            RAISERROR (@myErrorMessage, 11, @myErrorState)
-                WITH LOG;
-        END
-    IF @PrintAlertToConsole = 1
-        BEGIN
-            DECLARE @PrintMessage AS NVARCHAR (4000);
-            SET @PrintMessage = '------------' + @myNewLine + 'Alert Type: ' + @AlertType + @myNewLine + 'Alert Text: ' + CAST (@AlertText AS NVARCHAR (3600)) + @myNewLine + 'Alert Time: ' + CAST (@myAlertTime AS NVARCHAR (50));
-            PRINT @PrintMessage;
-        END
-    IF @ReturnAlertAsResultset = 1
-        BEGIN
-            SELECT @AlertType AS AlertType,
-                   @AlertText AS AlerText,
-                   @myAlertTime AS AkerTime;
-        END
-    IF @EmailAlert = 1
+	DECLARE @myAlertTime DATETIME;
+	DECLARE @myNewLine nvarchar(10);
+	SET @myAlertTime=GETDATE()
+	SET @myNewLine=CHAR(13)+CHAR(10)
+	SET @AlertType=ISNULL(@AlertType,N'UNKNOWN')
+	SET @AlertText=ISNULL(@AlertText,CAST(N'<UNKNOWN/>' AS XML))
+	SET @AlertText=CAST(N'<instance_name>' + CAST(@@SERVERNAME AS NVARCHAR(255)) + N'</instance_name>' + @myNewLine + CAST(@AlertText AS NVARCHAR(MAX)) AS XML)
+	--==============================================
+	--====================Log to table
+	--==============================================
+	IF @LogAlertToTable=1
+	BEGIN
+		INSERT INTO [trace].[Events]([AlertTime],[AlertType],[AlertText])
+		VALUES (@myAlertTime,@AlertType,@AlertText)
+	END
+	--==============================================
+	--====================Log to SQL Server log
+	--==============================================
+	IF @LogAlertToSqlLog=1
+	BEGIN
+		DECLARE @myErrorMessage nvarchar(4000)
+		DECLARE @myErrorState int
+		SET @myErrorMessage=@AlertType + N' on ' + CAST(@myAlertTime AS NVARCHAR(50)) + @myNewLine+ + CAST(@AlertText AS NVARCHAR(3600))
+		SET @myErrorState=1
+
+		RAISERROR (
+			@myErrorMessage, -- Message text.
+			11 ,--@ErrorSeverity, -- Severity.
+			@myErrorState -- State.
+			) WITH LOG;
+	END
+	--==============================================
+	--====================Print to console
+	--==============================================
+	IF @PrintAlertToConsole=1
+	BEGIN
+		DECLARE @PrintMessage nvarchar(4000)
+		SET @PrintMessage =
+			'------------' + @myNewLine+
+			'Alert Type: ' + @AlertType + @myNewLine+
+			'Alert Text: ' + CAST(@AlertText AS NVARCHAR(3600)) + @myNewLine+
+			'Alert Time: ' + CAST(@myAlertTime AS NVARCHAR(50))
+		Print @PrintMessage
+	END
+	--==============================================
+	--====================Return Alert As Resultset
+	--==============================================
+	IF @ReturnAlertAsResultset=1
+	BEGIN
+		SELECT
+			@AlertType AS AlertType,
+			@AlertText AS AlerText,
+			@myAlertTime AS AkerTime;
+	END
+
+	IF @EmailAlert = 1
        AND @MailList IS NOT NULL
        AND EXISTS (SELECT 1
                    FROM   [master].[sys].[configurations]
                    WHERE  [name] = 'Database Mail XPs'
                           AND CAST ([value_in_use] AS INT) = 1)
-        BEGIN
-            DECLARE @EmailMessage AS NVARCHAR (MAX);
-            SET @EmailMessage = 'Alert Type: ' + @AlertType + @myNewLine + 'Alert Text: ' + CAST (@AlertText AS NVARCHAR (MAX)) + @myNewLine + 'Alert Time: ' + CAST (@myAlertTime AS NVARCHAR (50));
-            EXECUTE msdb.dbo.[sp_send_dbmail] @recipients = @MailList, @subject = @AlertType, @body = @EmailMessage;
-        END
+	BEGIN
+		DECLARE @EmailMessage NVARCHAR(MAX)
+		SET @EmailMessage =
+			'Alert Type: ' + @AlertType + @myNewLine+
+			'Alert Text: ' + CAST(@AlertText AS NVARCHAR(MAX)) + @myNewLine+
+			'Alert Time: ' + CAST(@myAlertTime AS NVARCHAR(50))
+
+		EXEC msdb.dbo.[sp_send_dbmail] 
+			@recipients = @MailList,
+			@subject = @AlertType,
+			@body = @EmailMessage
+	END
 END
 
 GO
+-- Extended Properties
+
 EXEC sp_addextendedproperty N'Author', N'Siavash Golchoobian', 'SCHEMA', N'dbo', 'PROCEDURE', N'dbasp_alert_event', NULL, NULL
 GO
 EXEC sp_addextendedproperty N'Created Date', N'2017-03-13', 'SCHEMA', N'dbo', 'PROCEDURE', N'dbasp_alert_event', NULL, NULL
--- Stored Procedures/dbo.dbasp_get_error_info.sql
+++ Stored Procedures/dbo.dbasp_get_error_info.sql
@@ -1,52 +1,93 @@
-SET QUOTED_IDENTIFIER ON
-GO
-SET ANSI_NULLS ON
-GO
-CREATE PROCEDURE [dbo].[dbasp_get_error_info]
-@CustomMessage NVARCHAR (256)=NULL, @PrintOutput BIT=1, @ResultsetOutput BIT=0, @RaiseError BIT=1, @EmailError BIT=0, @MailTo VARCHAR (255)=NULL
+-- Stored Procedure
+
+-- =============================================
+-- Author:		<Golchoobian>
+-- Create date: <01/21/2015>
+-- Version:		<3.0.0.0>
+-- Description:	<Return Occured Error>
+-- Input Parameters:
+--	@CustomMessage		'...'	//Any custom message
+--	@PrintOutput:		0 or 1	//1 to Print output result or 0 to skip it
+--	@ResultsetOutput:	0 or 1	//1 to Return output result grid or 0 to skip it
+--	@RaiseError:		0 or 1	//1 to Raise an error or 0 to skip it
+-- =============================================
+CREATE PROCEDURE [dbo].[dbasp_get_error_info] (
+	@CustomMessage nvarchar(256)=Null,
+	@PrintOutput bit=1,
+	@ResultsetOutput bit=0,
+	@RaiseError bit=1,
+	@EmailError bit=0,
+	@MailTo VARCHAR(255)=NULL
+	)
 AS
 BEGIN
-    DECLARE @myNewLine AS NVARCHAR (10);
-    SET @myNewLine = CHAR(13) + CHAR(10);
-    IF @PrintOutput = 1
-        BEGIN
-            DECLARE @PrintMessage AS NVARCHAR (4000);
-            SET @PrintMessage = '------------' + @myNewLine + 'ErrorNumber: ' + ISNULL(CAST (ERROR_NUMBER() AS NVARCHAR), '') + @myNewLine + 'ErrorSeverity: ' + ISNULL(CAST (ERROR_SEVERITY() AS NVARCHAR), '') + @myNewLine + 'ErrorState: ' + ISNULL(CAST (ERROR_STATE() AS NVARCHAR), '') + @myNewLine + 'ErrorProcedure: ' + ISNULL(CAST (ERROR_PROCEDURE() AS NVARCHAR), '') + @myNewLine + 'ErrorLine: ' + ISNULL(CAST (ERROR_LINE() AS NVARCHAR), '') + @myNewLine + 'ErrorMessage: ' + ISNULL(CAST (ERROR_MESSAGE() AS NVARCHAR), '') + @myNewLine + 'Information: ' + ISNULL(@CustomMessage, '');
-            PRINT @PrintMessage;
-        END
-    IF @ResultsetOutput = 1
-        BEGIN
-            SELECT ERROR_NUMBER() AS ErrorNumber,
-                   ERROR_SEVERITY() AS ErrorSeverity,
-                   ERROR_STATE() AS ErrorState,
-                   ERROR_PROCEDURE() AS ErrorProcedure,
-                   ERROR_LINE() AS ErrorLine,
-                   ERROR_MESSAGE() AS ErrorMessage,
-                   @CustomMessage AS Information;
-        END
-    IF @RaiseError = 1
-        BEGIN
-            DECLARE @myErrorMessage AS NVARCHAR (4000);
-            DECLARE @myErrorState AS INT;
-            SET @myErrorMessage = N'CMS: ' + ERROR_MESSAGE() + @myNewLine + +'Information: ' + @CustomMessage;
-            SET @myErrorState = ERROR_STATE();
-            RAISERROR (@myErrorMessage, 11, @myErrorState)
-                WITH LOG;
-        END
-    IF @EmailError = 1
-       AND @MailTo IS NOT NULL
-       AND EXISTS (SELECT 1
-                   FROM   sys.configurations
-                   WHERE  NAME = 'Database Mail XPs'
-                          AND CAST ([value_in_use] AS INT) = 1)
-        BEGIN
-            DECLARE @EmailMessage AS NVARCHAR (4000);
-            SET @EmailMessage = '------------' + @myNewLine + 'ErrorNumber: ' + ISNULL(CAST (ERROR_NUMBER() AS NVARCHAR), '') + @myNewLine + 'ErrorSeverity: ' + ISNULL(CAST (ERROR_SEVERITY() AS NVARCHAR), '') + @myNewLine + 'ErrorState: ' + ISNULL(CAST (ERROR_STATE() AS NVARCHAR), '') + @myNewLine + 'ErrorProcedure: ' + ISNULL(CAST (ERROR_PROCEDURE() AS NVARCHAR), '') + @myNewLine + 'ErrorLine: ' + ISNULL(CAST (ERROR_LINE() AS NVARCHAR), '') + @myNewLine + 'ErrorMessage: ' + ISNULL(CAST (ERROR_MESSAGE() AS NVARCHAR), '') + @myNewLine + 'Information: ' + ISNULL(@CustomMessage, '');
-            EXECUTE msdb.dbo.[sp_send_dbmail] @recipients = @MailTo, @subject = @CustomMessage, @body = @EmailMessage;
-        END
+	DECLARE @myNewLine nvarchar(10);
+	SET @myNewLine=CHAR(13)+CHAR(10)
+
+	IF @PrintOutput=1
+		BEGIN
+			DECLARE @PrintMessage nvarchar(4000)
+			SET @PrintMessage =
+				'------------' + @myNewLine+
+				'ErrorNumber: ' + ISNULL(CAST(ERROR_NUMBER() as nvarchar),'') + @myNewLine+
+				'ErrorSeverity: ' + ISNULL(CAST(ERROR_SEVERITY() as nvarchar),'') + @myNewLine+
+				'ErrorState: ' + ISNULL(CAST(ERROR_STATE() as nvarchar),'') + @myNewLine+
+				'ErrorProcedure: ' + ISNULL(CAST(ERROR_PROCEDURE() as nvarchar),'') + @myNewLine+
+				'ErrorLine: ' + ISNULL(CAST(ERROR_LINE() as nvarchar),'') + @myNewLine+
+				'ErrorMessage: ' + ISNULL(CAST(ERROR_MESSAGE() as nvarchar),'')+ @myNewLine+
+				'Information: ' + ISNULL(@CustomMessage,'')
+			Print @PrintMessage
+		END
+
+	IF @ResultsetOutput=1
+		BEGIN
+			SELECT
+				ERROR_NUMBER() AS ErrorNumber
+				,ERROR_SEVERITY() AS ErrorSeverity
+				,ERROR_STATE() AS ErrorState
+				,ERROR_PROCEDURE() AS ErrorProcedure
+				,ERROR_LINE() AS ErrorLine
+				,ERROR_MESSAGE() AS ErrorMessage
+				,@CustomMessage AS Information;
+		END
+
+	IF @RaiseError=1
+		BEGIN
+			DECLARE @myErrorMessage nvarchar(4000)
+			DECLARE @myErrorState int
+			SET @myErrorMessage=N'CMS: ' + ERROR_MESSAGE() + @myNewLine+ + 'Information: ' + @CustomMessage
+			SET @myErrorState=ERROR_STATE()
+
+			RAISERROR (
+				@myErrorMessage, -- Message text.
+				11 ,--@ErrorSeverity, -- Severity.
+				@myErrorState -- State.
+				) WITH LOG;
+		END
+
+	IF @EmailError=1 AND @MailTo IS NOT NULL AND EXISTS (SELECT 1 FROM sys.configurations WHERE NAME = 'Database Mail XPs' AND CAST ([value_in_use] AS INT) = 1)
+		BEGIN
+			DECLARE @EmailMessage nvarchar(4000)
+			SET @EmailMessage =
+				'------------' + @myNewLine+
+				'ErrorNumber: ' + ISNULL(CAST(ERROR_NUMBER() as nvarchar),'') + @myNewLine+
+				'ErrorSeverity: ' + ISNULL(CAST(ERROR_SEVERITY() as nvarchar),'') + @myNewLine+
+				'ErrorState: ' + ISNULL(CAST(ERROR_STATE() as nvarchar),'') + @myNewLine+
+				'ErrorProcedure: ' + ISNULL(CAST(ERROR_PROCEDURE() as nvarchar),'') + @myNewLine+
+				'ErrorLine: ' + ISNULL(CAST(ERROR_LINE() as nvarchar),'') + @myNewLine+
+				'ErrorMessage: ' + ISNULL(CAST(ERROR_MESSAGE() as nvarchar),'')+ @myNewLine+
+				'Information: ' + ISNULL(@CustomMessage,'')
+
+			EXEC msdb.dbo.[sp_send_dbmail] 
+				@recipients = @MailTo,
+				@subject = @CustomMessage,
+				@body = @EmailMessage
+		END
 END
 
 GO
+-- Extended Properties
+
 EXEC sp_addextendedproperty N'Author', N'Siavash Golchoobian', 'SCHEMA', N'dbo', 'PROCEDURE', N'dbasp_get_error_info', NULL, NULL
 GO
 EXEC sp_addextendedproperty N'Created Date', N'2015-01-21', 'SCHEMA', N'dbo', 'PROCEDURE', N'dbasp_get_error_info', NULL, NULL
