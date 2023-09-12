93759c4be01ac8fd1406acff9c5f9243e2f2c84d
Siavash Golchoobian <siavash.golchoobian@gmail.com>
2023-09-12 13:55:24 +0330

    Update SqlDeep_Capture_Depricated.sql

--- _Maintenance Tasks/Extended Events/SqlDeep_Capture_Depricated.sql
+++ _Maintenance Tasks/Extended Events/SqlDeep_Capture_Depricated.sql
@@ -5,7 +5,7 @@ ADD EVENT sqlserver.deprecation_announcement(
 ADD EVENT sqlserver.deprecation_final_support(
     ACTION(sqlserver.client_app_name,sqlserver.client_hostname,sqlserver.database_id,sqlserver.nt_username,sqlserver.session_id,sqlserver.sql_text))
 ADD TARGET package0.event_file(SET filename=N'U:\Databases\SqlDeep_Capture_Depricated.xel')
-WITH (MAX_MEMORY=4096 KB,EVENT_RETENTION_MODE=ALLOW_SINGLE_EVENT_LOSS,MAX_DISPATCH_LATENCY=30 SECONDS,MAX_EVENT_SIZE=0 KB,MEMORY_PARTITION_MODE=PER_NODE,TRACK_CAUSALITY=OFF,STARTUP_STATE=ON)
+WITH (MAX_MEMORY=1024 KB,EVENT_RETENTION_MODE=ALLOW_SINGLE_EVENT_LOSS,MAX_DISPATCH_LATENCY=30 SECONDS,MAX_EVENT_SIZE=0 KB,MEMORY_PARTITION_MODE=PER_NODE,TRACK_CAUSALITY=OFF,STARTUP_STATE=ON)
 GO
 
 ALTER EVENT SESSION [SqlDeep_Capture_Depricated] ON SERVER STATE = START;
