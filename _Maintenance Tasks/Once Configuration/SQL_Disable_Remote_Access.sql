da4b6f0c537e735b82de07e34f12b19384169ee1
Siavash Golchoobian <siavash.golchoobian@gmail.com>
2023-09-12 13:46:07 +0330

    Update SQL_Disable_Remote_Access.sql

--- _Maintenance Tasks/Once Configuration/SQL_Disable_Remote_Access.sql
+++ _Maintenance Tasks/Once Configuration/SQL_Disable_Remote_Access.sql
@@ -2,5 +2,6 @@ EXECUTE sp_configure 'show advanced options', 1;
 RECONFIGURE;
 EXECUTE sp_configure 'remote access', 0;
 RECONFIGURE;
-GO EXECUTE sp_configure 'show advanced options', 0;
+GO 
+EXECUTE sp_configure 'show advanced options', 0;
 RECONFIGURE;
