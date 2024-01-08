8acd4c43a43ef16fd15ef0162d12ba02f9aef846
Siavash Golchoobian <siavash.golchoobian@gmail.com>
2024-01-08 13:54:10 +0330

    Update SQL_RevokePublicPermissions.sql
    REVOKE EXEC ON OBJECT::master.dbo.xp_grantlogin TO Public;
    REVOKE EXEC ON OBJECT::master.dbo.xp_revokelogin TO Public;
    REVOKE EXEC ON OBJECT::master.dbo.xp_sprintf TO Public;
    REVOKE EXEC ON OBJECT::master.dbo.xp_sscanf TO Public;
    REVOKE EXEC ON OBJECT::master.dbo.xp_repl_convert_encrypt_sysadmin_wrapper TO Public;
    REVOKE EXEC ON OBJECT::master.dbo.xp_replposteor TO Public;

--- _Maintenance Tasks/Once Configuration/SQL_RevokePublicPermissions.sql
+++ _Maintenance Tasks/Once Configuration/SQL_RevokePublicPermissions.sql
@@ -24,6 +24,12 @@ REVOKE EXEC ON OBJECT::master.dbo.xp_dirtree TO Public;
 REVOKE EXEC ON OBJECT::master.dbo.xp_fileexist TO Public;
 REVOKE EXEC ON OBJECT::master.dbo.xp_fixeddrives TO Public;
 REVOKE EXEC ON OBJECT::master.dbo.xp_getnetname TO Public;
+REVOKE EXEC ON OBJECT::master.dbo.xp_grantlogin TO Public;
+REVOKE EXEC ON OBJECT::master.dbo.xp_revokelogin TO Public;
+REVOKE EXEC ON OBJECT::master.dbo.xp_sprintf TO Public;
+REVOKE EXEC ON OBJECT::master.dbo.xp_sscanf TO Public;
+REVOKE EXEC ON OBJECT::master.dbo.xp_repl_convert_encrypt_sysadmin_wrapper TO Public;
+REVOKE EXEC ON OBJECT::master.dbo.xp_replposteor TO Public;
 
 --This revokation cause, non sysadmin users can't connect via SSMS, in this case you need to grant bellow permission to that specific users:
 USE [master]
