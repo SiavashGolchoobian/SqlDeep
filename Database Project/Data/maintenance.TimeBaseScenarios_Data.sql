389156499c4964e1c1cac10f972a28fca9868af4
Siavash Golchoobian <siavash.golchoobian@gmail.com>
2024-10-16 10:17:11 +0330

    add static data

---
+++ Database Project/Data/maintenance.TimeBaseScenarios_Data.sql
@@ -0,0 +1,3 @@
+INSERT INTO [maintenance].[TimeBaseScenarios] ([ScenarioName], [Enabled]) VALUES (N'Clone Original Backup Scenario on qcsrvr', 1)
+INSERT INTO [maintenance].[TimeBaseScenarios] ([ScenarioName], [Enabled]) VALUES (N'SqlDeep Distribution base scenario for backups on LINUX 192.168.1.10', 1)
+INSERT INTO [maintenance].[TimeBaseScenarios] ([ScenarioName], [Enabled]) VALUES (N'SqlDeep Distribution base scenario for exports on LINUX 192.168.1.10', 1)
